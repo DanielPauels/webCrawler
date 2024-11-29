99bee0c3432aee9f1473c7f5739b15f2daf80122
Daniel Pauels <daniel-pauels@web.de>
2024-11-29 19:14:33 +0100

    Filling of DataStorage.java.
    The Class saves the crawled data to the local filesystem.

--- mycrawler/src/main/java/com/mycrawler/DataStorage.java
+++ mycrawler/src/main/java/com/mycrawler/DataStorage.java
@@ -1,5 +1,23 @@
 package com.mycrawler;
 
-public class DataStorage {
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 
+// Saves the crawled data
+public class DataStorage {
+	public void save(String url, String html) {
+		try {
+			//Generate Filename with the URL
+			String filename = url.replaceAll("[^a-zA-Z0-9]", "_") + ".html";
+			File file = new File("data/" + filename);
+			file.getParentFile().mkdirs(); //Generate path if not already generated
+			
+			FileWriter writer = new FileWriter(file);
+			writer.write(html); //Write the html in the file
+			writer.close();
+		} catch (IOException e) {
+			System.err.println("Error while saving: " + url + " - " + e.getMessage());
+		}
+	}
 }
