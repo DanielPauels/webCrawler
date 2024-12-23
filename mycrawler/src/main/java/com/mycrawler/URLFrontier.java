906da6593256a40fe372af98e9d903ea9e054374
Daniel Pauels <daniel-pauels@web.de>
2024-12-23 12:58:32 +0100

    Implementing the URLFrontier to manage the queue URLs to be crawled

--- mycrawler/src/main/java/com/mycrawler/URLFrontier.java
+++ mycrawler/src/main/java/com/mycrawler/URLFrontier.java
@@ -1,7 +1,36 @@
 package com.mycrawler;
 
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Set;
 
-
+// Manages a queue of URLs to be crawled.
 public class URLFrontier {
+    private Queue<URLDepthPair> queue; // The queue of URLs.
 
+    public URLFrontier() {
+        this.queue = new LinkedList<>(); // Initialize an empty queue.
+    }
+
+    // Add a URL with its crawl depth to the queue.
+    public void add(String url, int depth) {
+        queue.add(new URLDepthPair(url, depth));
+    }
+
+    // Add multiple URLs with the same depth to the queue.
+    public void addAll(Set<String> urls, int depth) {
+        for (String url : urls) {
+            add(url, depth);
+        }
+    }
+
+    // Retrieve the next URL from the queue.
+    public URLDepthPair poll() {
+        return queue.poll();
+    }
+
+    // Check if the queue is empty.
+    public boolean isEmpty() {
+        return queue.isEmpty();
+    }
 }
