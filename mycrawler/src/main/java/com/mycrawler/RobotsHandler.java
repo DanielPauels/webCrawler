6e5c3ea4b32259f4ff7042c5e189c4f69c83eabc
Daniel Pauels <daniel-pauels@web.de>
2024-12-23 12:52:06 +0100

    Implementing the RobotsHandler to check if crawling is allowed

--- mycrawler/src/main/java/com/mycrawler/RobotsHandler.java
+++ mycrawler/src/main/java/com/mycrawler/RobotsHandler.java
@@ -1,5 +1,21 @@
 package com.mycrawler;
 
-public class RobotsHandler {
+import java.io.IOException;
+import java.net.URL;
+import org.jsoup.Jsoup;
 
+//Checks robots.txt to see if crawling is allowed.
+public class RobotsHandler {
+	public boolean isAllowed(String urlString) {
+		try {
+			URL url = new URL(urlString);
+			String robotsURL = url.getProtocol() + "://" + url.getHost() + "/robots.txt";
+
+			String robotsTxt = Jsoup.connect(robotsURL).ignoreContentType(true).execute().body();
+			return !robotsTxt.contains("Disallow: " + url.getPath());
+		} catch (IOException e) {
+			System.err.println("Error reading robots.txt: " + urlString + " - " + e.getMessage());
+			return true; // Default to true if robots.txt is inaccessible.
+		}
+	}
 }
