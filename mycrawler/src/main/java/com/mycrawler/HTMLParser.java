3233a912d59385ff032f28bfd5b6bca79ef9acb2
Daniel Pauels <daniel-pauels@web.de>
2024-12-23 12:47:09 +0100

    Implementing of HTMLParser to parse the HTML pages and extract the links

--- mycrawler/src/main/java/com/mycrawler/HTMLParser.java
+++ mycrawler/src/main/java/com/mycrawler/HTMLParser.java
@@ -1,5 +1,25 @@
 package com.mycrawler;
 
-public class HTMLParser {
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 
+import java.util.HashSet;
+import java.util.Set;
+
+//Parses HTML pages and extracts links
+public class HTMLParser {
+	public Set<String> extractLinks(String html, String baseUrl){
+		Set<String> links = new HashSet<>(); //Set to store extracted links
+		Document doc = Jsoup.parse(html, baseUrl); //Parse the HTML
+		
+		//Find all <a> tags with href attributes
+		Elements anchorTags = doc.select("a[href]");
+		for (Element anchor : anchorTags) {
+			String link = anchor.absUrl("href"); //Converts relatice URLs to absloute
+			links.add(link); //Add the link to the set
+		}
+		return links; //Return the set of links
+	}
 }
