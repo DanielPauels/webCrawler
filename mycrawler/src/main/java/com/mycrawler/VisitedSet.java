5dfb0027086eeac958afecbefa9d10484400c80c
Daniel Pauels <daniel-pauels@web.de>
2024-12-23 13:01:09 +0100

    Revert "Implementing the Crawler class that orchestrates  the crawling process"
    This reverts commit 7f62e2eb69f66991916761bd479ee3137b5df842.

--- mycrawler/src/main/java/com/mycrawler/VisitedSet.java
+++ mycrawler/src/main/java/com/mycrawler/VisitedSet.java
@@ -1,28 +1,5 @@
 package com.mycrawler;
 
-import java.util.HashSet;
-import java.util.Set;
-
-// Tracks visited URLs using a Set.
 public class VisitedSet {
-    private Set<String> visited; // The set of visited URLs.
 
-    public VisitedSet() {
-        this.visited = new HashSet<>(); // Initialize an empty set.
-    }
-
-    // Add a URL to the visited set.
-    public void add(String url) {
-        visited.add(url);
-    }
-
-    // Check if a URL has already been visited.
-    public boolean contains(String url) {
-        return visited.contains(url);
-    }
-
-    // Get the total number of visited pages.
-    public int size() {
-        return visited.size();
-    }
 }
