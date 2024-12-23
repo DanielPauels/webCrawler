a27d604b26645c6fce5459ee6e79002bc40e635b
Daniel Pauels <daniel-pauels@web.de>
2024-12-23 13:36:26 +0100

    Implementing the Main with Github.com as example URL

--- mycrawler/src/main/java/com/mycrawler/Main.java
+++ mycrawler/src/main/java/com/mycrawler/Main.java
@@ -1,10 +1,13 @@
 package com.mycrawler;
 
 public class Main {
+    public static void main(String[] args) {
+        System.out.println("Starting the web crawler...");
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+        // Initialize the crawler with maxDepth = 2 and maxPages = 5
+        Crawler crawler = new Crawler(2, 5);
 
-	}
-
+        // Start crawling from a seed URL
+        crawler.start("https://www.github.com");
+    }
 }
